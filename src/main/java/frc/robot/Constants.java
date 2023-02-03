e0ded45f44315c9c6a8ebcbe9ebe73b5e1e67ce2
Hydro111 <hydrogenhone@gmail.com>
2023-02-02 18:22:13 -0700

    Configure project structure

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -13,7 +13,28 @@ package frc.robot;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
-  public static class OperatorConstants {
-    public static final int kDriverControllerPort = 0;
+  public static final class Joysticks {
+    public static final int primaryControllerPort = 0;
+    public static final int secondaryControllerPort = 1;
+  }
+
+
+  public static final class Drivetrain {
+
+  }
+
+
+  public static final class Arm {
+
+  }
+
+
+  public static final class Grabber {
+
+  }
+
+
+  public static final class PhotonVision {
+    
   }
 }
