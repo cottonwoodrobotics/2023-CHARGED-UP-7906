90a3e9d3fcebcdc6bc5185eb08b618f4daaa6a39
Mothandman <117953018+Mothandman@users.noreply.github.com>
2023-02-10 12:34:55 -0700

    Added odometry and kinematics

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,9 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.MecanumDriveKinematics;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -16,17 +19,26 @@ package frc.robot;
 public final class Constants {
 
   //&& These are the Drivetrain motors.
-
+  //&& TODO: Add the motor IDs
   public static final int driveTrainIDFrontLeft = 0;
   public static final int driveTrainIDFrontRight = 0;
   public static final int driveTrainIDBackLeft = 0;
   public static final int driveTrainIDBackRight = 0;
 
   //&& This is the Drive speed multiplier
-
   public static final double driveSpeedMultiplier = 0;
+
+  //&& Lengths of the front/back and side(s) of the chassis
+  //&& TODO: Measure side lengths of chassis. Do this wheel-to-wheel
+  public static final double driveTrainFrontLength = 0;
+  public static final double driveTrainSideLength = 0;
+
+  public static final MecanumDriveKinematics driveKinematics = new MecanumDriveKinematics(new Translation2d(driveTrainFrontLength / 2, driveTrainSideLength / 2),
+    new Translation2d(driveTrainFrontLength / 2, -driveTrainSideLength / 2), new Translation2d(-driveTrainFrontLength / 2, driveTrainSideLength / 2),
+    new Translation2d(-driveTrainFrontLength / 2, -driveTrainSideLength / 2));
   
 }
 
 
 
+ 
--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -1,21 +1,53 @@
 package frc.robot.subsystems;
 
+
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.drive.MecanumDrive;
 import edu.wpi.first.wpilibj.drive.MecanumDrive.WheelSpeeds;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
+//Odometry & kinematics imports
+import com.kauailabs.navx.frc.AHRS;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.kinematics.MecanumDriveOdometry;
+import edu.wpi.first.math.kinematics.MecanumDriveWheelPositions;
+import edu.wpi.first.math.kinematics.MecanumDriveKinematics;
+
 public class DriveTrain extends SubsystemBase {
     //&& Creates DriveTrainSubsystem
-    public DriveTrain() {}
+    public DriveTrain() {
+        resetOdometry(new Pose2d());
+    }
 
+    //&& Motor objects
     CANSparkMax frontLeftMotor = new CANSparkMax(Constants.driveTrainIDFrontLeft, MotorType.kBrushless);
     CANSparkMax frontRightMotor = new CANSparkMax(Constants.driveTrainIDFrontRight, MotorType.kBrushless);
     CANSparkMax backLeftMotor = new CANSparkMax(Constants.driveTrainIDBackLeft, MotorType.kBrushless);
     CANSparkMax backRightMotor = new CANSparkMax(Constants.driveTrainIDBackRight, MotorType.kBrushless);
+
+    //&& Motor encoder objects
+    RelativeEncoder frontLeftEncoder = frontLeftMotor.getEncoder();
+    RelativeEncoder frontRightEncoder = frontRightMotor.getEncoder();
+    RelativeEncoder backLeftEncoder = backLeftMotor.getEncoder();
+    RelativeEncoder backRightEncoder = backRightMotor.getEncoder();
+
+    //&& Creates the gyro object
+    private AHRS m_gyro = new AHRS();
+
+    private MecanumDriveOdometry m_Odometry = new MecanumDriveOdometry(Constants.driveKinematics, m_gyro.getRotation2d(), gWheelPositions());
+
+    public MecanumDriveWheelPositions gWheelPositions() {
+        return new MecanumDriveWheelPositions(frontLeftEncoder.getPosition(), frontRightEncoder.getPosition(), backLeftEncoder.getPosition(), backRightEncoder.getPosition());
+    }
+
+    public void resetOdometry(Pose2d pose) {
+        m_Odometry.resetPosition(m_gyro.getRotation2d(), gWheelPositions(), pose);
+    }
 
     public void driveCartesian(double ySpeed, double xSpeed, double zRotation) {
         
@@ -28,15 +60,29 @@ public class DriveTrain extends SubsystemBase {
 
     }
 
+
+
+    public Pose2d getPose() {
+        return m_Odometry.getPoseMeters();
+    }
+
+
+
     @Override
     public void periodic() {
         //&& This method will be called once per scheduler run
+        //&& cool gay mer stuff
+        m_Odometry.update(m_gyro.getRotation2d(), gWheelPositions());
     }
 
     @Override
     public void simulationPeriodic() {
         //&& This method will be called once per scheduler run DURING SIMULATION
     }
+
+
+
+    
 }
 
 
